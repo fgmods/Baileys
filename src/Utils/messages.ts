7691cee1f48aad642f9d31efb99c72c08a1e0f26
FG98 <112351085+fgmods@users.noreply.github.com>
2022-11-10 13:03:43 -0400

    Update messages.ts

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -1,5 +1,5 @@
 import { Boom } from '@hapi/boom'
-import axios from 'axios'
+import axios from 'axios' 
 import { promises as fs } from 'fs'
 import { Logger } from 'pino'
 import { proto } from '../../WAProto'
